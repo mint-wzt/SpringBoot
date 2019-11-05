8ab1de8534df23597b315f3ac5efec2c3ae04c5b
wzt <1479605082@qq.com>
2019-11-05 16:05:36 +0800

    使用JPA 的基本用法

---
+++ src/main/java/com/wzt/springboot/springboot/repository/PersonRepository.java
@@ -0,0 +1,35 @@
+package com.wzt.springboot.springboot.repository;
+
+import com.wzt.springboot.springboot.model.Person;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface PersonRepository extends JpaRepository<Person,Long> {
+    Optional<Person> findByName(String name);
+
+    List<Person> findByAgeGreaterThan(int age);
+
+    @Query("select p from Person p where p.name = :name")
+    Optional<Person> findByNameCustomeQuery(@Param("name") String name);
+
+    @Query("select p.name from Person p where p.id = :id")
+    String findPersonNameById(@Param("id") Long id);
+
+    /**
+     * 自定义 sql 语句更新操作
+     */
+    @Modifying
+    @Transactional
+    @Query("update Person p set p.name = ?1 where p.id = ?2")
+    void updatePersonNameById(String name, Long id);
+
+
+}
