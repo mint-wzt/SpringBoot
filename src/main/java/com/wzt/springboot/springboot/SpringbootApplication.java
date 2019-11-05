907e2807b621a9ceeb705f12620ebaeb68efc8de
wzt <1479605082@qq.com>
2019-11-05 18:03:11 +0800

    添加手动实现的过滤器Filter

--- src/main/java/com/wzt/springboot/springboot/SpringbootApplication.java
+++ src/main/java/com/wzt/springboot/springboot/SpringbootApplication.java
@@ -2,8 +2,10 @@ package com.wzt.springboot.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 
 @SpringBootApplication
+@ServletComponentScan
 public class SpringbootApplication {
     public static void main(String[] args) {
         SpringApplication.run(SpringbootApplication.class, args);
---
+++ src/main/java/com/wzt/springboot/springboot/filter/MyFilterConfig.java
@@ -0,0 +1,39 @@
+package com.wzt.springboot.springboot.filter;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+@Configuration
+public class MyFilterConfig {
+    @Autowired
+    MyFilter myFilter;
+
+    @Autowired
+    MyFilter2 myFilter2;
+
+    @Bean
+    public FilterRegistrationBean<MyFilter> setUpMyFilter(){
+        FilterRegistrationBean<MyFilter> filterFilterRegistrationBean = new FilterRegistrationBean<>();
+        filterFilterRegistrationBean.setOrder(2);
+        filterFilterRegistrationBean.setFilter(myFilter);
+        filterFilterRegistrationBean.setUrlPatterns(new ArrayList<>(Arrays.asList("/api/*")));
+        return filterFilterRegistrationBean;
+    }
+
+    @Bean
+    public FilterRegistrationBean<MyFilter2> setUpMyFilter2(){
+        FilterRegistrationBean<MyFilter2> filterFilterRegistrationBean = new FilterRegistrationBean<>();
+        filterFilterRegistrationBean.setOrder(1);
+        filterFilterRegistrationBean.setFilter(myFilter2);
+        filterFilterRegistrationBean.setUrlPatterns(new ArrayList<>(Arrays.asList("/api/*")));
+        return filterFilterRegistrationBean;
+    }
+
+
+
+}
