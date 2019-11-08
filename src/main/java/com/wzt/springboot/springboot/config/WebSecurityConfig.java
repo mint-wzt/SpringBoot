453193f894d8c9e6c8dfbb2a2e1bce2279614d66
wzt <1479605082@qq.com>
2019-11-08 10:17:24 +0800

    简单的Spring Security

--- src/main/java/com/wzt/springboot/springboot/config/WebSecurityConfig.java
+++ src/main/java/com/wzt/springboot/springboot/config/WebSecurityConfig.java
@@ -1,38 +1,35 @@
 package com.wzt.springboot.springboot.config;
 
-import com.wzt.springboot.springboot.service.UserService;
-import org.springframework.context.annotation.Bean;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.UserDetailsService;
+
 
 @Configuration
+@EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
-
-    @Bean
-    UserDetailsService customUserService(){
-        return new UserService();
-    }
-
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.userDetailsService(customUserService());
-    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
-                .anyRequest()
-                .authenticated()
+               .antMatchers("/","home").permitAll()
+                .anyRequest().authenticated()
                 .and()
                 .formLogin()
                 .loginPage("/login")
-                .failureUrl("/login?error")
                 .permitAll()
                 .and()
                 .logout()
                 .permitAll();
+    }
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth
+                .inMemoryAuthentication()
+                .withUser("admin").password("admin").roles("USER");
     }
 }
